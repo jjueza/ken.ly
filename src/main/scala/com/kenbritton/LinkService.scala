5d37e4109b5f0e233d5995a200a6e5df6e5554c4
Ken Britton <kenbritton@gmail.com>
2013-09-22 12:32:35 -0700

    generate hash values; send 404's if hash not found; index lookup fields

--- src/main/scala/com/kenbritton/LinkService.scala
+++ src/main/scala/com/kenbritton/LinkService.scala
@@ -9,6 +9,7 @@ import DefaultJsonProtocol._
 import spray.http._
 import HttpMethods._
 import com.mongodb.casbah.Imports._
+import HashidsJava._
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
@@ -30,21 +31,23 @@ trait LinkService extends HttpService {
 	val mongoClient =  MongoClient("localhost", 27017)
 	val db = mongoClient("links")
 	val hashColl = db("hashes")
+	hashColl.ensureIndex(MongoDBObject("hash" -> 1))
+	val hashids = new Hashids("ken.ly.super.secure.salt", 8);
 	
 	val route = {
 		get {
 			path("actions" / "hash") {
 				parameter("url") { url =>
+					
+					val hash = hashids.encrypt(java.lang.Math.abs(url.hashCode))
+					val count = 0: java.lang.Integer
+					val doc = MongoDBObject()
+					doc += "url" -> url
+					doc += "hash" -> hash
+					doc += "count" -> count
+					hashColl.insert(doc)
+					
 					respondWithMediaType(`application/json`) { 
-						
-						val hash = "12345" //"%d".format(url.hashCode)
-						val count = 0: java.lang.Integer
-						val doc = MongoDBObject()
-						doc += "url" -> url
-						doc += "hash" -> hash
-						doc += "count" -> count
-						hashColl.save(doc)
-						
 						complete {
 							s"""{"originalURL":"${url}","shortenedURL":"ken.ly/${hash}"}"""
 						}
@@ -53,18 +56,40 @@ trait LinkService extends HttpService {
 			} ~
 			path("actions" / "stats") {
 				parameter("hash") { hash =>
-					respondWithMediaType(`application/json`) { 
-						val count = hashColl.findOne(MongoDBObject("hash" -> hash)).get("count")
-						complete {
-							s"""{"shortenedURL":"ken.ly/${hash}","clickCount":"${count}"}"""
-						}
+					
+					val doc = hashColl.findOne(MongoDBObject("hash" -> hash))
+					doc match {
+						case Some(doc) =>
+							respondWithMediaType(`application/json`) { 
+								complete {
+									s"""{"shortenedURL":"ken.ly/${hash}","clickCount":"${doc.get("count")}"}"""
+								}
+							}
+						case None =>
+							respondWithMediaType(`application/json`) { 
+								respondWithStatus(StatusCodes.NotFound) {
+									complete {
+										s"""{"error":"No stats available for the requested URL"}"""
+									}
+								}
+							}
+					
 					}
 				}
 			} ~ 
-			path("[\\w\\d]{5}".r) { hash =>
-				val url = hashColl.findOne(MongoDBObject("hash" -> hash)).get("url")
-				println(url.toString)
-				redirect(url.toString, StatusCodes.TemporaryRedirect)
+			path("[\\w\\d]{8,}".r) { hash =>
+				val doc = hashColl.findOne(MongoDBObject("hash" -> hash))
+				doc match {
+					case Some(doc) =>
+						hashColl.update(MongoDBObject("hash" -> hash), $inc("count" -> 1))
+						redirect(doc.get("url").toString, StatusCodes.TemporaryRedirect)
+					case None =>
+						respondWithStatus(StatusCodes.NotFound) {
+							complete {
+								"The requested URL could not be found"
+							}
+						}
+				}
 			}
 		}
 	}
